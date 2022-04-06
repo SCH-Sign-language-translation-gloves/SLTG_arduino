baf309a669467fe07272e02afc57fc9764b2ad49
Jiyoung <thdwldud8@gmail.com>
2022-04-06 16:57:06 +0900

    wifi 연결에 필요한 코드 업데이트

--- sensor/FirebaseIoT/config.h
+++ sensor/FirebaseIoT/config.h
@@ -1,7 +1,6 @@
 
-#define WIFI_SSID "ML518"
-#define WIFI_PASSWORD "518518518"
+#define WIFI_SSID "IoT417"
+#define WIFI_PASSWORD "iot10041004"
 
 #define DEVICE_ID "TestDevice"
-#define PROJECT_ID "test2"
-//#define PROJECT_ID "est2-7a801-default-rtdb.firebaseio.com"
+#define PROJECT_ID "test2-7a801-default-rtdb.firebaseio.com"
