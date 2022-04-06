5e30b1e6bf037fd50b7c9490bf13d7fc53bcd0bf
Jiyoung <thdwldud8@gmail.com>
2022-04-06 21:30:07 +0900

    firebase 연결 작업 코드

---
+++ sensor/FirebaseIoT/firecast/functions/src/index.ts
@@ -0,0 +1,6 @@
+import * as functions from "firebase-functions";
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+  console.log("Hello");
+  response.send("Hello from Firebase!");
+});
