a50dd305974f32ef171ebba4a1af165e580ec2a7
Jiyoung <thdwldud8@gmail.com>
2022-04-20 21:59:40 +0900

    firebase test2 배포완료

--- sensor/FirebaseIoT/firecast/functions/src/index.ts
+++ sensor/FirebaseIoT/firecast/functions/src/index.ts
@@ -1,6 +1,6 @@
 import * as functions from "firebase-functions";
 
 export const helloWorld = functions.https.onRequest((request, response) => {
-  console.log("Hello");
-  response.send("Hello from Firebase!");
-});
+    console.log("Hello")
+   response.send("Hello from Firebase!");
+ });
