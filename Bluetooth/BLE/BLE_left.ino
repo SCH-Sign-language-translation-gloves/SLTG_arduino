2d56b5b3c96925d01f832207a60d5237aec9c965
Jiyoung <thdwldud8@gmail.com>
2022-05-11 21:19:01 +0900

    블루투스 코드 수정 및 폴더 정리

--- Bluetooth/BLE/BLE_left.ino
+++ Bluetooth/BLE/BLE_left.ino
@@ -3,26 +3,26 @@
 
 BLEService sensorService("66df5109-edde-4f8a-a5e1-02e02a69cbd5");
 //자이로(각속도)
-BLEStringCharacteristic xSensorLevel("741c12b9-e13c-4992-8a5e-fce46dec0bff", BLERead | BLENotify,15);
-BLEStringCharacteristic ySensorLevel("baad41b2-f12e-4322-9ba6-22cd9ce09832", BLERead | BLENotify,15);
-BLEStringCharacteristic zSensorLevel("5748a25d-1834-4c68-a49b-81bf3aeb2e50", BLERead | BLENotify,15);
+BLEStringCharacteristic xSensorLevel("120d8460-76cc-4ad1-b3d7-ebdbf681ca38", BLERead | BLENotify,15);
+BLEStringCharacteristic ySensorLevel("9dc7f95f-ec49-47c5-ad76-36af6293a5e4", BLERead | BLENotify,15);
+BLEStringCharacteristic zSensorLevel("cef9bf69-6af9-43c9-a3b8-941881518666", BLERead | BLENotify,15);
 //가속도
-BLEStringCharacteristic axSensorLevel("3aae57bc-efe5-4a17-8f24-f610b1b985e5", BLERead | BLENotify,15);
-BLEStringCharacteristic aySensorLevel("7ad8f8f6-2da4-45a1-adbd-ab5a83351426", BLERead | BLENotify,15);
-BLEStringCharacteristic azSensorLevel("7cac39b1-89e0-47be-aaf4-30277f05d763", BLERead | BLENotify,15);
+BLEStringCharacteristic axSensorLevel("86fef35c-b14a-4705-a901-3153c8f029b9", BLERead | BLENotify,15);
+BLEStringCharacteristic aySensorLevel("99a8424c-0c9b-45c3-a702-c2c08012f762", BLERead | BLENotify,15);
+BLEStringCharacteristic azSensorLevel("e0684334-3382-467c-bc3f-26ee1ee0b624", BLERead | BLENotify,15);
 //Flex
-BLEStringCharacteristic flex1Sensor("a9d33c96-c5cf-41c6-bec4-3e28305ae1ab", BLERead | BLENotify,15);
-BLEStringCharacteristic flex2Sensor("112170b3-c9f0-4f4c-84fa-d440ce80ff06", BLERead | BLENotify,15);
-BLEStringCharacteristic flex3Sensor("442b3f31-8d10-494d-bef4-85c0faa9d28a", BLERead | BLENotify,15);
-BLEStringCharacteristic flex4Sensor("29e632b7-f055-4daa-beb3-f66635a38ad2", BLERead | BLENotify,15);
-BLEStringCharacteristic flex5Sensor("00481010-9c4e-4ab5-9e37-d2641d6c7755", BLERead | BLENotify,15);
+BLEStringCharacteristic flex1Sensor("3627b31e-fd54-4fe5-8059-fb723ccfa54c", BLERead | BLENotify,15);
+BLEStringCharacteristic flex2Sensor("ba63eb9d-129d-4024-9607-8108fdd9299b", BLERead | BLENotify,15);
+BLEStringCharacteristic flex3Sensor("ab4a7f9e-00b4-4454-a22e-732236b2c1e1", BLERead | BLENotify,15);
+BLEStringCharacteristic flex4Sensor("48ea1dcb-e94c-4158-89a1-c4a4d7265a4c", BLERead | BLENotify,15);
+BLEStringCharacteristic flex5Sensor("368e79cc-5280-41dd-a0a4-575cc5385703", BLERead | BLENotify,15);
 
 //flex sensor pin
-int flexpin_5 = A7;
-int flexpin_4 = A3;
+int flexpin_5 = A0;
+int flexpin_4 = A1;
 int flexpin_3 = A2;
-int flexpin_2 = A1;
-int flexpin_1 = A0;
+int flexpin_2 = A3;
+int flexpin_1 = A7;
 
 // last sensor data
 float oldXLevel = 0;
@@ -75,89 +75,73 @@ void setup() {
  Serial.println("Bluetooth device active, waiting for connections...");
 }
 
-void updateGyroscopeLevel() {
+void updateLevel() {
  float x, y, z;
- if (IMU.gyroscopeAvailable()) {
+ float ax, ay, az;
+ int flex1 = analogRead(flexpin_1);  //센서값을 저장할 변수 설정
+ int flex2 = analogRead(flexpin_2); 
+ int flex3 = analogRead(flexpin_3);
+ int flex4 = analogRead(flexpin_4);
+ int flex5 = analogRead(flexpin_5);
+ 
+ if (IMU.gyroscopeAvailable()& IMU.accelerationAvailable()) {
   IMU.readGyroscope(x, y, z);
+  IMU.readAcceleration(ax, ay, az);
+  
   if (x != oldXLevel) {
-   xSensorLevel.writeValue(String(x));
    oldXLevel = x;
   }
  if (y != oldYLevel) {
-  ySensorLevel.writeValue(String(y));
   oldYLevel = y;
  }
  
  if (z != oldZLevel) {
-  zSensorLevel.writeValue(String(z));
   oldZLevel = z;
   }
+  
+ if (ax != a_oldXLevel) {
+  a_oldXLevel = ax;
+   }
+  
+ if (ay != a_oldYLevel) {
+    a_oldYLevel = ay;
+   }
+  
+ if (az != a_oldZLevel) {
+    a_oldZLevel = az;
+   }
+  
  Serial.print("Gyro :");
  Serial.print(x);
  Serial.print('\t');
  Serial.print(y);
  Serial.print('\t');
  Serial.println(z);
+ Serial.print("Acceleration :");
+ Serial.print(ax);
+ Serial.print('\t');
+ Serial.print(ay);
+ Serial.print('\t');
+ Serial.println(az);
  }
-}
- 
- void updateAccelerationLevel() {
- float ax, ay, az;
- 
- if (IMU.accelerationAvailable()) {
-  IMU.readAcceleration(ax, ay, az);
-
-   if (ax != a_oldXLevel) {
-  axSensorLevel.writeValue(String(ax));
-  a_oldXLevel = ax;
-   }
-  
-   if (ay != a_oldYLevel) {
-    aySensorLevel.writeValue(String(ay));
-    a_oldYLevel = ay;
-   }
-  
-   if (az != a_oldZLevel) {
-    azSensorLevel.writeValue(String(az));
-    a_oldZLevel = az;
-   }
-  Serial.print("Acceleration :");
-  Serial.print(ax);
-  Serial.print('\t');
-  Serial.print(ay);
-  Serial.print('\t');
-  Serial.println(az);
-  }
- }
- void updateflexsensor(){
-  int flex1 = analogRead(flexpin_1);  //센서값을 저장할 변수 설정
-  int flex2 = analogRead(flexpin_2); 
-  int flex3 = analogRead(flexpin_3);
-  int flex4 = analogRead(flexpin_4);
-  int flex5 = analogRead(flexpin_5);
-  
   if (flex1 != old_flex1) {
-  flex1Sensor.writeValue(String(flex1));
   old_flex1 = flex1;
    }
   
   if (flex2 != old_flex2) {
-  flex2Sensor.writeValue(String(flex2));
   old_flex2 = flex2;
    }
   
   if (flex3 != old_flex3) {
-  flex3Sensor.writeValue(String(flex3));
   old_flex3 = flex3;
    }
    
   if (flex4 != old_flex4) {
-  flex4Sensor.writeValue(String(flex4));
   old_flex4 = flex4;
    }
    
   if (flex5 != old_flex5) {
-  flex5Sensor.writeValue(String(flex5));
+  flex5Sensor.writeValue("LEFT HAND"+"\t"+String(x)+"\t"+String(y)+"\t"+String(z)+"\t"+String(ax)+"\t"+String(ay)+"\t"+String(az)+"\t"+String(flex1)+'\t'+String(flex2)+'\t'+String(flex3)+'\t'+String(flex4)+'\t'+String(flex5));
   old_flex5 = flex5;
    }
   
@@ -171,20 +155,18 @@ void updateGyroscopeLevel() {
   Serial.print(flex4);
   Serial.print('\t');
   Serial.println(flex5);
-  
- }
+}
  
  void loop() {
  BLEDevice central = BLE.central();
+ 
  if (central) {
   Serial.print("Connected to central: ");
   Serial.println(central.address());
 
   while (central.connected()) {
    //long currentMillis = millis();
-   updateGyroscopeLevel();
-   updateAccelerationLevel();
-   updateflexsensor();
+   updateLevel();
    delay(300);
   }
 
