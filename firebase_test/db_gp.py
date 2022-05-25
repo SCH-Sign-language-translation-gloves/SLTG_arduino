f383c2bf5f6f8aaf17304b5b349986e080a21909
Jiyoung <thdwldud8@gmail.com>
2022-05-26 01:15:43 +0900

    flex 평균 값 int형으로 변환하여 올리기

--- firebase_test/db_gp.py
+++ firebase_test/db_gp.py
@@ -1,3 +1,4 @@
+from typing import final
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
@@ -30,12 +31,19 @@ zyro_z = db.reference('sensor_data').child('zyro z').get()
 #firebase에서 받은 데이터들을 다차원배열로 넣기
 fire_list = [accel_x, accel_y, accel_z, zyro_x, zyro_y, zyro_z, flex_1, flex_2, flex_3, flex_4, flex_5]
 
-#문자열로 받은 데이터들(fire_list)을 float로 바꿔주기 위한 코드
+#문자열로 받은 데이터들(fire_list)에서 각속도, 가속도 센서 데이터들만 float로 바꿔주기 위한 코드
 sensor_list = []
-for i in range(len(fire_list)):
+for i in range(6):
     N = []
-    for j in range(len(fire_list[i])):
+    for j in range(len(fire_list)):
         N.append(float(fire_list[i][j]))
+    sensor_list.append(N)
+
+#문자열로 받은 데이터들(fire_list)에서 flex센서 데이터들만 int로 바꿔주기 위한 코드
+for a in range(6, 11):
+    N = []
+    for j in range(len(fire_list)):
+        N.append(int(fire_list[a][j]))
     sensor_list.append(N)
 
 # zyro_x 데이터 sensor_list[3]
@@ -44,7 +52,7 @@ interval_data = [[],[],[],[],[],[],[],[],[],[],[]]
 count = 0
 for i in range(len(sensor_list[3])): #section_list의 크기만큼 for문 동작
     if sensor_list[3][i] >= 22.0: #zyro_x가 일정 수치보다 크거나 같으면 카운드 +1
-        count += 1
+         count += 1
     if count == 1: #count가 1이면 데이터를 g_list에 저장
         for j in range(len(interval_data)):
             interval_data[j].append(sensor_list[j][i])
@@ -53,11 +61,14 @@ for i in range(len(sensor_list[3])): #section_list의 크기만큼 for문 동작
 
 #지화 모드 시 final_data에 있는 각각의 센서값들의 평균을 구하는 코드
 final_data = [[],[],[],[],[],[],[],[],[],[],[]]
-for f in range(len(interval_data)): #각 배열의 0번째 값 삭제(시작 값)
+for f in range(6): #각 배열의 0번째 값 삭제(시작 값)
     interval_data[f].pop(0)
-
     final_data[f].append(round(np.mean(interval_data[f]), 2))
 
+#flex센서들은 평균을 구하고 int로 자료형 변환을 시켜주기
+for t in range(6, 11):
+    interval_data[t].pop(0)
+    final_data[t].append(int(np.mean(interval_data[t])))
 print(final_data)
 
 dir = db.reference()
@@ -65,7 +76,7 @@ dir.update({'입력된 센싱값 평균': final_data[0] + final_data[1] + final_
  + final_data[6] + final_data[7] + final_data[8] + final_data[9] + final_data[10]})
 
 
-##실시간 그래프를 그리는 코드
+#실시간 그래프를 그리는 코드
 #def animate(i):
 #    plt.cla()
 #    plt.plot(f_list, 'g', linewidth = 0.5)
